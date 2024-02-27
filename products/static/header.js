7563f4ab6c46081505eb5f709b38a82106e0c48e
Gio Kelly <gakellyg@unal.edu.co>
2024-02-27 07:51:28 -0600

    fixed the collapsible sidebar
    fixed the collapsible sidebar that make the the search bar move when it was opened

--- products/static/header.js
+++ products/static/header.js
@@ -1,10 +1,10 @@
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.display = "none";
+    document.getElementById("main").style.visibility = "hidden";
   }
   
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.display= "flex";
+    document.getElementById("main").style.visibility= "visible";
   }
