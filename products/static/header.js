6d43159a828010ee2893703c18b2e3767c46c358
Gio Kelly <gakellyg@unal.edu.co>
2024-02-26 20:06:32 -0600

    create header Component
    Create a like header component and inject it on the main templates using the django tag 'include'

---
+++ products/static/header.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.display= "flex";
+  }
--- products/static/index.js
+++ products/static/index.js
@@ -1,10 +1,1 @@
 
-function openNav() {
-  document.getElementById("mySidebar").style.width = "400px";
-  document.getElementById("main").style.display = "none";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.display= "flex";
-}
