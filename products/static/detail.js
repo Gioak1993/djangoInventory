8d4aed6f99dbef759d689442cf76a0a487b2f137
Gio Kelly <gakellyg@unal.edu.co>
2024-02-27 22:13:16 -0600

    updated side bar
    Side bar updated

--- products/static/detail.js
+++ products/static/detail.js
@@ -1,14 +1,3 @@
 function changeMainImage(imageUrl) {
     document.getElementById('main-image').innerHTML = '<img src="' + imageUrl + '" alt="Main Image">';
 }
-
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "400px";
-    document.getElementById("main").style.display = "none";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.display= "flex";
-  }
--- products/static/header.js
+++ products/static/header.js
@@ -1,6 +1,6 @@
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.width = "100%";
     document.getElementById("main").style.visibility = "hidden";
   }
   
